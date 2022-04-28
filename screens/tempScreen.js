be750fbe409aac547e091727f2d125baa0e7b063
Rahul Negi <rahul.negi.9078@gmail.com>
2022-04-28 16:22:38 +0530

    second commit

---
+++ screens/tempScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const tempScreen = () => {
+  return (
+    <View>
+      <Text>tempScreen</Text>
+    </View>
+  )
+}
+
+export default tempScreen
