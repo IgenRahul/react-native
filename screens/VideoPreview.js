87a438a1053fde6c67882255dc91db4fac291de9
codefactor-io <support@codefactor.io>
2022-06-01 20:56:31 +0000

    [CodeFactor] Apply fixes

--- screens/VideoPreview.js
+++ screens/VideoPreview.js
@@ -1,28 +1,28 @@
-import { View, Text } from 'react-native'
+import {View, Text} from 'react-native';
 import React from 'react';
 import VideoPlayer from 'react-native-video-controls';
-import { useRoute } from '@react-navigation/native';
+import {useRoute} from '@react-navigation/native';
 
 const VideoPreview = () => {
-
-    const parameter = useRoute().params.data;
+  const parameter = useRoute().params.data;
   return (
-    <View style={{
-        flex : 1,
-    }}>
+    <View
+      style={{
+        flex: 1,
+      }}>
       <VideoPlayer
-                source={{
-                     uri: parameter
-                // uri : 'https://www.w3schools.com/html/mov_bbb.mp4'
-                 }}
-                onBack={()=>{
-                    navigation.goBack();
-                }}
-                paused = {true}
-                title = 'preview'
-            />
+        source={{
+          uri: parameter,
+          // uri : 'https://www.w3schools.com/html/mov_bbb.mp4'
+        }}
+        onBack={() => {
+          navigation.goBack();
+        }}
+        paused={true}
+        title="preview"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default VideoPreview
+export default VideoPreview;
